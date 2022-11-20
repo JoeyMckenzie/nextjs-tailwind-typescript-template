9b72e2e5c23f64a0cfb82faaac03eb1c54bf0aff
Joey McKenzie <joey.mckenzie27@gmail.com>
2022-11-20 11:08:12 -0800

    feat(workspace): migrate to nextjs 13

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['tailwindui.com', 'images.unsplash.com'],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
